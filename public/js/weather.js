aea936b6036212bb7b5f3880cef78cee0d1d4a2c
你的 Github 用户名 < Github >
2024-05-06 18:31:24 +0800

    1

---
+++ public/js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "0124",
+        "background": "1",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "1",
+        "language": "auto",
+        "borderRadius": "7",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "150",
+        "top": "10",
+        "key": "7f7e9cb370a245a8a0a682086ae7578e"
+    }
+}
