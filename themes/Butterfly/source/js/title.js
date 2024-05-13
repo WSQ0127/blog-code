2a06cc1d8b42b2c7f425cbc2cd332fb9d82cae98
你的 Github 用户名 < Github >
2024-05-13 22:42:57 +0800

    1

---
+++ themes/Butterfly/source/js/title.js
@@ -0,0 +1,15 @@
+var OriginTitile=document.title;
+var titleTime;
+document.addEventListener('visibilitychange',function()
+{
+    if (document.hidden)
+    {
+        document.title="w(ﾟДﾟ)w 不要走！再看看嘛！";
+        clearTimeout(titleTime);
+    }
+    else
+    {
+    document.title="♪(^∇^*)欢迎肥来！";
+    titleTime=setTimeout(function(){document.title=OriginTitile;},2000);
+    }
+});
